37a233f17dbc865d8354e8442c68cd33ee70b326
B.L.A.C.C <blaccm797@gmail.com>
2025-06-12 19:26:47 +0200

    creation of directories in js

---
+++ Java Script/01_fundamentals/01-Conditions/largest_number.js
@@ -0,0 +1,11 @@
+//write the larger number between the 2 numbers
+
+const a =12;
+const b=10;
+
+if(a>b){
+    console.log(`The larger number between the 2 is: ${a}`);
+}
+else{
+    console.log(`The larger number between the 2 is: ${b}`);
+}
